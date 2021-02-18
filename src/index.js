8a8927daa6954d0cf171d668726b25b83a8ed51b
olaoot <furin1998@gmail.com>
2021-02-18 20:53:29 +0900

    feat(theme): Setting ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { ThemeProvider } from 'styled-components'
+import theme from './style/theme'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
   document.getElementById('root')
 );
